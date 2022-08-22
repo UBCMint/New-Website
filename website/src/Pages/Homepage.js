42d7bf2778bb821c83a43b76e9e7b9108ab516b3
Priyanshu Mahey <72003945+priyanshumahey@users.noreply.github.com>
2022-08-22 08:31:15 -0700

    Logos and Manifest Update
    Added logos, favicon, web manifest

--- website/src/Pages/Homepage.js
+++ website/src/Pages/Homepage.js
@@ -1,6 +1,11 @@
 function Homepage () {
     return(
-      <p>Homepage Component</p>
+      <div className='Homepage'>
+        <h1>Homepage Components</h1>
+        <h2>Header 2</h2>
+        <h3>Header 3</h3>
+        <p>Paragraph</p>
+      </div>
     )
   }
 
