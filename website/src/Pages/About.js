b8215e8c82a8284617626543458af1cabeb40a0a
Stephanie Quon <stephanie.quon@alumni.ubc.ca>
2022-10-02 11:53:32 -0700

    starting UI for about us page

--- website/src/Pages/About.js
+++ website/src/Pages/About.js
@@ -1,7 +1,28 @@
-function About () {
-    return(
-      <p>About Component</p>
-    )
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+
+function About(props) {
+    const { classes } = props;
+  
+    return (
+      <React.Fragment>
+        <CssBaseline />
+        <div className={classes.layout}>
+
+            <Typography
+              component="h2"
+              variant="h5"
+              color="inherit"
+              align="center"
+              noWrap
+              className={classes.toolbarTitle}
+            >
+              Blog
+            </Typography>
+      </div>
+      </React.Fragment>
+    );
   }
 
 export default About;
